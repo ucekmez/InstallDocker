9cb098ea21ff7438090f3cc1ea49ed18a5a0ab08
Ugur Cekmez <ugur.cekmez@siemens.com>
2019-02-07 14:37:19 +0300

    initialize script

---
+++ install.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+import os, subprocess, sys
+
+def run_command(command):
+    if sys.version_info[0] < 3: # python 2x
+        return subprocess.check_output(command, shell=True).strip().decode()
+    else: # python 3x
+        return subprocess.run(command.split(), stdout=subprocess.PIPE).stdout.decode()
+
+# get system information
+# architecture
+architecture = run_command("uname -m").replace("\n", "")
+# OS name. # possible OSes : debian, ubuntu, centos
+OS           = run_command("lsb_release -is").replace("\n", "").lower()
+# OS version # cosmic, bionic, stretch, etc...
+CODENAME     = run_command("lsb_release -cs").replace("\n", "")
+
+
+run_command('sudo apt-get remove docker docker-engine docker.io containerd runc')
+run_command('sudo apt-get update')
+
+if OS == 'debian':
+    run_command('sudo apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common')
+elif OS == 'ubuntu':
+    run_command('sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common')
+
+run_command('curl -fsSL https://download.docker.com/linux/{}/gpg | sudo apt-key add -'.format(OS))
+
+if architecture in ['x86_64', 'amd64']:
+    run_command('sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/{} {} stable"'.format(OS, CODENAME))
+else:
+    run_command('sudo add-apt-repository "deb [arch={}] https://download.docker.com/linux/{} {} stable"'.format(architecture, OS, CODENAME))
+
+run_command('sudo apt-get update')
+run_command('sudo apt-get install docker-ce docker-ce-cli containerd.io')
+run_command('sudo groupadd docker')
+run_command('sudo usermod -aG docker $USER')
+run_command('docker run hello-world')
