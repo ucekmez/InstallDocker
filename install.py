53ccdc11848497a7c1b65843ecc29eebe8722595
Ugur Cekmez <ugur.cekmez@siemens.com>
2019-02-07 14:53:58 +0300

    fix

--- install.py
+++ install.py
@@ -18,30 +18,30 @@ CODENAME     = run_command("lsb_release -cs").replace("\n", "")
 print ("arch: {}\nOS: {}\ncodename: {}".format(architecture, OS, CODENAME))
 
 print('previous installations are being removed...', end='\r')
-run_command('sudo apt remove docker docker-engine docker.io containerd runc')
+os.system('sudo apt remove docker docker-engine docker.io containerd runc')
 
 print('system is being updated...', end='\r')
-run_command('sudo apt update')
+os.system('sudo apt update')
 
 print('dependencies are being installed...', end='\r')
 if OS == 'debian':
-    run_command('sudo apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common')
+    os.system('sudo apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common')
 elif OS == 'ubuntu':
-    run_command('sudo apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common')
+    os.system('sudo apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common')
 
-run_command('curl -fsSL https://download.docker.com/linux/{}/gpg | sudo apt-key add -'.format(OS))
+os.system('curl -fsSL https://download.docker.com/linux/{}/gpg | sudo apt-key add -'.format(OS))
 
 print('repository is being added...', end='\r')
 if architecture in ['x86_64', 'amd64']:
-    run_command('sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/{} {} stable"'.format(OS, CODENAME))
+    os.system('sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/{} {} stable"'.format(OS, CODENAME))
 else:
-    run_command('sudo add-apt-repository "deb [arch={}] https://download.docker.com/linux/{} {} stable"'.format(architecture, OS, CODENAME))
+    os.system('sudo add-apt-repository "deb [arch={}] https://download.docker.com/linux/{} {} stable"'.format(architecture, OS, CODENAME))
 
 
 print('docker is finally being installed...', end='\r')
-run_command('sudo apt update')
-run_command('sudo apt install -y docker-ce docker-ce-cli containerd.io')
+os.system('sudo apt update')
+os.system('sudo apt install -y docker-ce docker-ce-cli containerd.io')
 print('permissions are being updating...', end='\r')
-run_command('sudo groupadd docker')
-run_command('sudo usermod -aG docker {}'.format(getpass.getuser()))
-run_command('docker run hello-world')
+os.system('sudo groupadd docker')
+os.system('sudo usermod -aG docker {}'.format(getpass.getuser()))
+os.system('docker run hello-world')
